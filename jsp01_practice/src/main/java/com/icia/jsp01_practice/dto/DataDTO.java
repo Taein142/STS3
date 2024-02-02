27a4cf3e6aa1ac23f528ddda2e31bc861e0e5ce5
Taein142 <xodlsdldy@naver.com>
2024-02-02 14:44:41 +0900

    20240201부터 시작한 스프링(레거시)

---
+++ jsp01_practice/src/main/java/com/icia/jsp01_practice/dto/DataDTO.java
@@ -0,0 +1,14 @@
+package com.icia.jsp01_practice.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+public class DataDTO {
+	private String dataName;
+	private int dataAge;
+	private String dataNumber;
+}
